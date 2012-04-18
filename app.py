7331bcf29bbb6a8af8f838799dbb1a13a2b1cabb
Paul Chakravarti <paul.chakravarti@gmail.com>
2012-04-18 15:04:40 +0100

    working

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import os
 
 from urlparse import urlparse
 from peewee import PostgresqlDatabase
-from flask import Flask,request,response
+from flask import Flask,request
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG',False)
 SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-db_params = urlparse(os.environ.get('DATABASE_URL',"postgres://localhost/paulc"))
+db_params = urlparse(os.environ.get('DATABASE_URL'))
 
 db = PostgresqlDatabase(database=db_params.path[1:],
                         user=db_params.username,
@@ -22,16 +22,18 @@ db.connect()
 
 @app.route('/')
 def version():
-    response.headers["Content-type"] = "text/plain"
-    return db.execute('select version()').fetchone()[0]
+    return (db.execute('select version()').fetchone()[0],200,{'Content-type':'text/plain'})
 
 @app.route('/oauth')
 def oauth():
     r = []
-    for k,v in request.values:
+    for k,v in request.values.iteritems():
         r.append("Key: %s / Value: %s" % (k,v)) 
-    response.headers["Content-type"] = "text/plain"
-    return "\n".join(r)
+    return ("\n".join(r),200,{'Content-type':'text/plain'})
+
+@app.route('/error')
+def error():
+    raise Exception("An error")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
